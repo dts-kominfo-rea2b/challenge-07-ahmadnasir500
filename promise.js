5386696d9f299bb6de98f690e5349738b397be95
ahmadnasir500 <ahmad.nsr500@gmail.com>
2022-06-12 23:30:38 +0700

    soal promise

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (sentiment) => {
+  try {
+    // simpan array 
+    const [resIXX, resVGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    // gabung array
+    const result = [...resIXX, ...resVGC]
+
+    // filter result
+    return result.filter(item => item.hasil === sentiment).length || 0
+  } catch (error) {
+    console.log("there was an error: " + error);
+  }
+}
 
 module.exports = {
   promiseOutput,
